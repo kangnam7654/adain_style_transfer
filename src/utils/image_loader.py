188428f2f9378ba039512a0b42f64e19ff19f370
Imcloud_Kangnam <kangnam@imcloud.co.kr>
2022-07-20 18:31:45 +0900

    fix: 경로 입력과 numpy 입력 둘 다 받도록 수정

--- src/utils/image_loader.py
+++ src/utils/image_loader.py
@@ -1,7 +1,13 @@
-from PIL import Image
+import numpy as np
 import torch
+from PIL import Image
 
-def image_loader(image_name, transform, device):
-    image = Image.open(image_name)
+def image_loader(image, transform, device):
+    if type(image) == str:
+        image = Image.open(image)
+    elif type(image) == np.ndarray:
+        image = Image.fromarray(image)
+    else:
+        raise Exception(f'wrong input "{image}"; \n path or cv2 image(numpy) needed')
     image = transform(image).unsqueeze(0)  # 네트워크의 입력 차원에 맞추기 위해 필요한 가짜 배치 차원
     return image.to(device, torch.float)
