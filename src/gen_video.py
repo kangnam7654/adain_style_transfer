61eb2f745f0427f2744780f780a7b4df46511843
Kangnam Kim <kangnam7654@naver.com>
2022-07-21 13:56:34 +0900

    codec load 부분 분리

--- src/gen_video.py
+++ src/gen_video.py
@@ -9,6 +9,7 @@ import torch
 from utils.adain import adaptive_instance_normalization
 from utils.load_transform import load_transform
 from utils.image_loader import image_loader
+from utils.load_codec import load_codec
 from model.vgg19_encoder import vgg19_encoder
 from model.vgg19_decoder import vgg19_decoder
 
@@ -19,23 +20,6 @@ warnings.filterwarnings('ignore')
 ROOT_DIR = Path(__file__).parent
 
 
-def load_codec(device, config):
-    encoder = vgg19_encoder().to(device)
-    decoder = vgg19_decoder().to(device)
-
-    encoder_pt_path = config['main']['encoder_pt_path']
-    decoder_pt_path = config['main']['decoder_pt_path']
-
-    encoder_pt = torch.load(encoder_pt_path)
-    decoder_pt = torch.load(decoder_pt_path)
-
-    encoder.load_state_dict(encoder_pt, strict=False)
-    decoder.load_state_dict(decoder_pt)
-
-    encoder = encoder.eval()
-    decoder = decoder.eval()
-    return encoder, decoder
-
 def style_transfer(encoder, decoder, content_input, style_input, alpha=1.0):
     assert (0.0 <= alpha <= 1.0)
     content_feature = encoder(content_input)
@@ -44,6 +28,10 @@ def style_transfer(encoder, decoder, content_input, style_input, alpha=1.0):
     feature = feature * alpha + content_feature * (1 - alpha) # Alpha가 1에 가까울수록 스타일이 진해짐
     return decoder(feature)
 
+def tensor2cv(input_tensor):
+    cv_image = input_tensor.squeeze(0).mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
+    return cv_image
+
 def main():
     with open(ROOT_DIR.joinpath('config', 'config.yaml'), 'r') as f:
         config = yaml.full_load(f)
@@ -51,23 +39,26 @@ def main():
     device = config['main']['device']
 
     # image 불러오기
-    content_path = 'D:\project\style_transfer\AdaIN_style_transfer\src\data\sample.mp4' # video
-    style_path = config['main']['style_image']
+    content_path = config['main']['content'] # video
+    style_path = config['main']['style']
     transform = load_transform() # 이미지 변환 load
+    
+    cap = cv2.VideoCapture(content_path) # Content Video
     style_input = image_loader(style_path, transform=transform, device=device) # [1, C, H, W] torch.tensor
 
-    out = cv2.VideoWriter(filename='test2.mp4', fourcc=cv2.VideoWriter_fourcc(*'DIVX'), fps=25, frameSize=(512,512))
-    cap = cv2.VideoCapture(content_path)
+    fourcc = cv2.VideoWriter_fourcc(*config['main']['fourcc'])
+    out = cv2.VideoWriter(fourcc=fourcc, **config['main']['video_writer']) # Video 저장 객체  
+    
     assert(cap.isOpened())
-    encoder, decoder = load_codec(device, config)
+    encoder, decoder = load_codec(device=device, **config['main']['load_codec'])
 
     while True:
         ret, frame = cap.read()
         if ret:
             content = image_loader(frame, transform, device)
-            st = style_transfer(encoder, decoder, content_input=content, style_input=style_input, alpha=0.5)
-            st = st.squeeze(0).mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to("cpu", torch.uint8).numpy()
-            out.write(st)
+            st = style_transfer(encoder, decoder, content_input=content, style_input=style_input, alpha=0.8)
+            out.write(tensor2cv(st))
+            # cv2.imshow('test', st)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
@@ -75,7 +66,7 @@ def main():
 
     cap.release()
     out.release()
-    # Style Transfer
+
     
 if __name__ == '__main__':
     # parser = argparse.ArgumentParser()
