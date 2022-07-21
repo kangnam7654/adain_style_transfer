1dfc4a83a406123b1f87ca8decaf4b30a4fe90fa
Kangnam Kim <kangnam7654@naver.com>
2022-07-21 13:57:02 +0900

    enc dec load 분리

---
+++ src/utils/load_codec.py
@@ -0,0 +1,19 @@
+import torch
+
+from model.vgg19_encoder import vgg19_encoder
+from model.vgg19_decoder import vgg19_decoder
+
+
+def load_codec(encoder_pt_path, decoder_pt_path, device):
+    encoder = vgg19_encoder().to(device)
+    decoder = vgg19_decoder().to(device)
+
+    encoder_pt = torch.load(encoder_pt_path)
+    decoder_pt = torch.load(decoder_pt_path)
+
+    encoder.load_state_dict(encoder_pt, strict=False)
+    decoder.load_state_dict(decoder_pt)
+
+    encoder = encoder.eval()
+    decoder = decoder.eval()
+    return encoder, decoder
