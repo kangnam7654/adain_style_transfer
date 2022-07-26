8413301dcff509662b75223b39519eb658fb9e19
Imcloud_Kangnam <kangnam@imcloud.co.kr>
2022-07-26 16:47:36 +0900

    fix: 입력에서 받던 type별 함수 분리

--- src/utils/image_loader.py
+++ src/utils/image_loader.py
@@ -1,13 +1,19 @@
-import numpy as np
+import io
 import torch
 from PIL import Image
 
 def image_loader(image, transform, device):
-    if type(image) == str:
-        image = Image.open(image)
-    elif type(image) == np.ndarray:
-        image = Image.fromarray(image)
-    else:
-        raise Exception(f'wrong input "{image}"; \n path or cv2 image(numpy) needed')
+    image = Image.open(image)
     image = transform(image).unsqueeze(0)  # 네트워크의 입력 차원에 맞추기 위해 필요한 가짜 배치 차원
     return image.to(device, torch.float)
+
+
+def cv2pil(cv2_image, transform, device):
+    image = Image.fromarray(cv2_image)
+    image = transform(image).unsqueeze(0)
+    return image.to(device, torch.float)
+
+
+def io_image_open(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes)) # PIL Image
+    return image
