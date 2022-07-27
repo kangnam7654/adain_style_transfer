4d2937f6bc1dc8ddd0298fa2c3b0970fb1b82095
Imcloud_Kangnam <kangnam@imcloud.co.kr>
2022-07-27 17:51:13 +0900

    minor fix

--- src/app.py
+++ src/app.py
@@ -1,17 +1,17 @@
 import base64
 from pathlib import Path
 
+ROOT_DIR = Path(__file__).parents[0]
+
 import torch
 from torchvision.utils import save_image
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 
-from model.style_transfer import AdaIN_transfer
 from utils.image_loader import image_loader
 from utils.load_transform import load_transform
 from utils.load_codec import load_codec
+from model.style_transfer import AdaIN_transfer
 
-
-ROOT_DIR = Path(__file__).parent
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 encoder_pt_path = ROOT_DIR.joinpath('model', 'weights', 'vgg19_encoder.pt')
 decoder_pt_path = ROOT_DIR.joinpath('model', 'weights', 'vgg19_decoder.pt')
@@ -19,15 +19,17 @@ save_path = ROOT_DIR.joinpath('data')
 encoder, decoder = load_codec(encoder_pt_path=encoder_pt_path, decoder_pt_path=decoder_pt_path, device=device)
 transform = load_transform()
 
-
-
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 @app.route('/upload', methods=['GET'])
-def upload_page():
+def upload():
     if request.method == 'GET':
-        return render_template("1_upload.html")
+        return render_template("upload.html")
 
 @app.route("/inference", methods=['POST'])
 def inference():
@@ -37,6 +39,7 @@ def inference():
 
         con_save_path = save_path.joinpath(content_input.filename).absolute()
         stl_save_path = save_path.joinpath(style_input.filename).absolute()
+        
         result_file_name = f'{content_input.filename.split(".")[0]}_{style_input.filename.split(".")[0]}.jpg'
         result_save_path = ROOT_DIR.joinpath('result', result_file_name)
 
@@ -48,8 +51,10 @@ def inference():
         output = AdaIN_transfer(encoder, decoder, content, style, 0.8)
         save_image(output, result_save_path)
         with open(result_save_path, 'rb') as f:
-            image_b64 = base64.b64encode(f.read())
-        return image_b64
+            image_b64 = base64.b64encode(f.read()) # base64 format
+        b64_image = base64.b64encode(image_b64).decode('utf-8')
+        result_json = {'image': b64_image}
+        return jsonify(result_json)
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True)
---
+++ src/utils/string2image.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+import base64
+import io
+from PIL import Image
+import cv2
+import numpy as np
+
+def stringToRGB(base64_string):
+    imgdata = base64.b64decode(base64_string)
+    dataBytesIO = io.BytesIO(imgdata)
+    image = Image.open(dataBytesIO)
+    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
